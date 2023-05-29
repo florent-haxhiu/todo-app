2b56b9afe4592012e1bf40ff56d05edcf4a4aff0
florent-haxhiu <florent_h2@hotmail.com>
2023-05-29 14:55:03 +0100

    First FT specced out in comments, now uses unittest.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+from unittest import TestCase, main
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert "The" in browser.title
+class NewVisitorTest(TestCase):
+
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # User goes to check out new to-do app
+        self.browser.get("http://localhost:8000")
+
+        # User notices the page title and header mention to-do lists
+        self.assertIn("To-Do", self.browser.title)
+        self.fail('Finish the test!')
+
+        # User is invited to enter a to-do item straight away
+
+
+if __name__ == 'main':
+    main()
