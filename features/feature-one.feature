1913584c666ce7db061748fc759280cf0465da18
florent-haxhiu <florent_h2@hotmail.com>
2023-06-14 07:22:39 +0100

    Feature works

---
+++ features/feature-one.feature
@@ -0,0 +1,5 @@
+Feature: Add Todo
+  Scenario: Add a todo item and show up on page.
+    Given User creates todo item
+    When User creates todo item
+    Then Todo item shows up on page
---
+++ features/steps/feature-one.py
@@ -0,0 +1,9 @@
+from behave import given
+
+@given('User creates todo item')
+def user_creates_todo_item():
+    pass
+
+@then('Todo item shows up on page')
+def todo_shows_up_on_page:
+    pass
